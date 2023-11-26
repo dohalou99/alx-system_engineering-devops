20e1370bf9e3028161626bceebecc2a308a92757
Youssef-At <youssefataifa0212@gmail.com>
2023-11-26 13:21:15 -0800

    hard project

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#configure ssh config
+file_line { 'no password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+file_line { 'change private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school'
+}

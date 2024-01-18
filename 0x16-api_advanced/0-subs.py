aa4ac437ea899cfa1e978171e1084be2f3fd5e3a
Youssef-At <youssefataifa0212@gmail.com>
2024-01-18 12:16:21 -0800

    push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""query subscribers on a given Reddit subreddit."""
+"""queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
 
 import requests
 
